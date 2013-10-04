cddfc244d8a97288a0e39bcbc099af8ae6423b40
Gustavo <correia.gustavol@gmail.com>
2013-10-05 00:35:34 +1000

    Created the userReader

--- src/edu/unsw/comp9321/assign2/UserBean.java
+++ src/edu/unsw/comp9321/assign2/UserBean.java
@@ -13,6 +13,74 @@ public class UserBean {
 	private String creditCard;
 	private String role;
 	private boolean locked;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickmane() {
+		return nickmane;
+	}
+	public void setNickmane(String nickmane) {
+		this.nickmane = nickmane;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastNameString() {
+		return lastNameString;
+	}
+	public void setLastNameString(String lastNameString) {
+		this.lastNameString = lastNameString;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(String creditCard) {
+		this.creditCard = creditCard;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public boolean isLocked() {
+		return locked;
+	}
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+	
+	
 	
 
 }
--- src/edu/unsw/comp9321/assign2/UserReader.java
+++ src/edu/unsw/comp9321/assign2/UserReader.java
@@ -1,5 +1,50 @@
 package edu.unsw.comp9321.assign2;
 
+import java.sql.*;
+
+/*
+ * Reads Users from the database and returns ResultSets
+ * 
+ */
 public class UserReader {
+
+	ConnectionManager cm;
+	Connection c;
+	String memberQuery = "SELECT * FROM member";
+	PreparedStatement query;
+
+	public UserReader() {
+		cm = new ConnectionManager();
+		c = cm.getConnection();
+		try {
+			query = c.prepareStatement(memberQuery);
+		} catch (SQLException s) {
+			System.out.println("Error preparing statment");
+			s.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Return all the member from the DB
+	 */
+	public ResultSet getAllMembers() {
+		ResultSet set = null;
+		try {
+			set = query.executeQuery();
+			if (set == null) {
+				System.out.println("No rows returned");
+				return null;
+			}
+			else {
+				System.out.println("returning results");
+				return set;
+			}
+		}
+		catch (SQLException s) {
+			System.out.println("Error executing query");
+			s.printStackTrace();
+		}
+		return set;
+	}
 
 }
