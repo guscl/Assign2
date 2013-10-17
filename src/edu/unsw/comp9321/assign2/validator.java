c2d24fc4a2085872e4003d23027f835a66e75eea
Mattw46 <matthew_w46@yahoo.com.au>
2013-10-17 12:45:20 +1100

    Update validator.java
    Additional methods and bugs in some methods fixed

--- src/edu/unsw/comp9321/assign2/validator.java
+++ src/edu/unsw/comp9321/assign2/validator.java
@@ -10,6 +10,7 @@ public class validator {
 	private static Pattern pattern;
 	private static Matcher matcher;
 	
+	// used to check username and other values with no spaces
 	public static boolean checkText(String text) {
 		System.out.println("validation string: " + text);
 		String regex = "[^=*][a-zA-Z0-9]*"; // work for user 1
@@ -26,17 +27,37 @@ public class validator {
 		return matcher.matches();
 	}
 	
-	public static boolean checkNumSequence(String sequence) { // credit card [0-9]{16}
-		String regex = "[0-9]";
+	public static boolean checkCreditCard(String sequence) { 
+		String regex = "[0-9]{16}";
 		pattern = Pattern.compile(regex);
 		matcher = pattern.matcher(sequence);
 		return matcher.matches();
 	}
 	
-	public static boolean checkURL(String url) {
-		String regex = "^[a-z]*@[a-z.]*";
+	public static boolean checkEmail(String email) {
+		String regex = "^[a-z0-9]*@[a-z.]*";
 		pattern = Pattern.compile(regex);
-		matcher = pattern.matcher(url);
+		matcher = pattern.matcher(email);
 		return matcher.matches();
+	}
+	
+	public static boolean checkYear(int year) {
+		if (year > 1900 && year < 2099) {
+			return true;
+		}
+		return false;
+	}
+	
+	// can be used for any long text sequence, allows space
+	public static boolean checkLongText(String text) {
+		System.out.println("validation string: " + text);
+		String regex = "[^=*][a-zA-Z0-9 ]*"; // work for user 1
+		pattern = Pattern.compile(regex);
+		matcher = pattern.matcher(text);
+		return matcher.matches();
+	}
+	
+	public static boolean checkURL(String url) {
+		return true;
 	}
 }
